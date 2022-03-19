9044e3eaeebeb250d9a1557a7d513f49c69ba134
prodigy07 <Luan_henrique21@hotmail.com>
2022-03-19 15:45:06 -0300

    update js

--- script.js
+++ script.js
@@ -6,20 +6,24 @@ const cardThanks = document.querySelector('.c2')
 
 let value = 0
 
-function toggleShow() {
-  buttonSubmit.addEventListener('click', () => {
-    cards[0].classList.remove('show')
-    cards[1].classList.add('show')
-  })
+function validationSubmit() {
+    if (!value == 0) {
+      cards[0].classList.remove('show')
+      cards[1].classList.add('show')
+    } else {
+      alert('Select a number!')
+    }
 }
 
-buttonMenu.forEach((index, card) => {
+buttonSubmit.addEventListener('click', validationSubmit)
+
+buttonMenu.forEach(index => {
   index.addEventListener('click', () => {
     buttonMenu.forEach(index => index.classList.remove('active'))
     cardThanks.classList.add('transition')
     value = index.dataset.value
     index.classList.add('active')
-    toggleShow()
     textInfo.innerHTML = `You selected ${value} out of 5`
   })
 })
+
